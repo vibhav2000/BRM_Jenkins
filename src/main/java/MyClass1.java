2251cdab33e7050f86bd532156694223368d7b82
Vibhav <vibhav.sharma2000@gmail.com>
2020-05-13 13:42:55 +0530

    initial commit

---
+++ src/main/java/MyClass1.java
@@ -0,0 +1,12 @@
+
+public class MyClass1 {
+
+	public static void main(String[] args) {
+	
+		System.out.println("Hello");
+		System.out.println("This is new mavn Project");
+		System.out.println("This my new java class ");
+		
+	}
+
+}
